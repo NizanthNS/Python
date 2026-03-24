1713f34afca1c7c537ff6bbb6c502a9c3e2727e3
Nizanth NS <nizanthns10@gmail.com>
2026-03-24 22:31:11 +0530

    Add files via upload

---
+++ 03. Typecasting.py
@@ -0,0 +1,28 @@
+# Typecasting = the process of converting a variable from one data type to another
+#             = str(), int, float(), bool()
+
+name = "Niz"
+age = 27
+CGPA = 8.6
+is_student = False
+
+# print(type(name))
+# print(type(age))
+# print(type(CGPA))
+# print(type(is_student))
+
+# CGPA = int(CGPA)
+# print(CGPA)
+
+# age = float(age)
+# print(age)
+
+#age = str(age)
+#print(type(age))
+
+#age = str(age)
+#age += "1"
+#print(age)
+
+name = bool(name) # see if the string is empty or not
+print(name)
---
+++ 04. Input().py
@@ -0,0 +1,30 @@
+#input() = A function that prompts the user to enter data
+#          Returns the entered data as a string
+
+
+#name = input("What is your name? : ")
+#age =int(input("What is your age? : "))
+
+#age = int(age)
+#age = age - 1
+
+#print(f"Hello {name}!")
+#print("HAPPY BIRTHDAY!")
+#print(f"You are {age} years old.")
+
+# Exercise 1 Rectangle Area Calculation
+
+#length = float(input("Enter the length: "))
+#width = float(input("Enter the width: "))
+#area = length * width
+
+#print(f"The area is: {area}cm²")
+
+item = input("What item do you like?: ")
+price = float(input("What is the price of the item?: "))
+quantity = int(input("How many items do you like?: "))
+total = price * quantity
+
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: ${total}")
+
