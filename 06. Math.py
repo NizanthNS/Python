5dad60f3e9338ca13f8c955bf630b72e4939b228
Nizanth NS <nizanthns10@gmail.com>
2026-03-25 22:57:57 +0530

    Add files via upload

---
+++ 06. Math.py
@@ -0,0 +1,67 @@
+# friends = 10
+
+# friends = friends + 1
+# friends += 1
+# friends = friends - 1
+# friends -=2
+# friends = friends * 2
+# friends *= 3
+# friends = friends / 2
+# friends /= 2
+# friends = friends ** 2 -- Square
+# friends **= 2
+# remainder = friends % 3
+
+# print(remainder)
+
+
+# x = 3.14
+# y = 4
+# z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4, 3)
+# result = max(x, y, z)
+# result = min(x, y, z)
+
+# print(result)
+
+
+# import math
+
+# x = 9
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(x)
+# result = math.ceil(x) -- Number up - float
+# result = math.floor(x) -- Number Down - float
+
+# print(result)
+
+# import math
+
+# radius = float(input("Enter a radius of a circle: "))
+
+# circumference = 2 * math.pi * radius
+
+# print(f"The circumference of the circle is {round(circumference, 2)}cm²")
+
+# import math
+
+# radius = float(input("Enter the radius of a circle: "))
+
+# area = math.pi * pow(radius, 2)
+
+# print(f"The area of a circle is {round(area, 2)}cm²")
+
+import math
+
+a = float(input("Enter a side A: "))
+b = float(input("Enter a side B: "))
+
+c =  math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(f"The Side C is {round(c)}")
+
