64d8327b1efc5fdd57d245745f41866a600528f1
Nizanth NS <nizanthns10@gmail.com>
2026-03-26 22:26:24 +0530

    Add files via upload

---
+++ 07. if statements.py
@@ -0,0 +1,41 @@
+# if = Do some code only IF some condition is True
+#      Else do something else
+
+# age = int(input("Enter your age: "))
+
+
+# if age >= 100:
+#    print("You are too old to walk")
+# elif age >= 18:
+#    print("You are old enough to vote")
+# elif age < 0:
+#    print("You are not born yet")
+# else:
+#    print("You are not old enough to vote")
+
+# response = input("Would you like food? (Y/N): ")
+# if response == "Y":
+#    print("Have some food")
+# else:
+#    print("No food for you")
+
+# name = input("Enter your name: ")
+
+# if name == "":
+#    print("Your name is empty")
+# else:
+#    print(f"Your name is {name}")
+
+# for_sale = False
+
+# if for_sale:
+#     print("This item is for sale")
+# else:
+#    print("This item is not for sale")
+
+online = False
+
+if online:
+    print("The user is online")
+else:
+    print("The user is not online")
