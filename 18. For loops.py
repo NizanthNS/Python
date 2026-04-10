de0f6c2aa3bed98876eda5f12b6b8ff76d343d64
Nizanth NS <nizanthns10@gmail.com>
2026-04-10 22:57:42 +0530

    Add files via upload

---
+++ 18. For loops.py
@@ -0,0 +1,29 @@
+# for loops = execute a block of code a fixed number of times.
+#             You can iterate over a range, string, sequence, etc.
+
+# for x in range(1, 11):
+#     print(x)
+
+# for x in reversed(range(1, 11)):
+#     print(x)
+# print("HAPPY BIRTHDAY")
+
+# for x in range(1, 11, 2):
+#     print(x)
+
+# credit = "1234-1256-5443-3455"
+#
+# for x in credit:
+#     print(x)
+
+# for x in range(1, 21):
+#     if x == 13:
+#         continue
+#     else:
+#         print(x)
+
+for x in range(1, 21):
+    if x == 13:
+        break
+    else:
+        print(x)
