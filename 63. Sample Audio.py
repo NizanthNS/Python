1b77d0340c94051b15defcc828bc392bdb1d9c5a
Nizanth NS <nizanthns10@gmail.com>
2026-06-04 21:31:49 +0530

    Add files via upload

---
+++ 63. Sample Audio.py
@@ -0,0 +1,14 @@
+# Python Alarm Clock
+
+import sounddevice as sd
+import soundfile as sf
+
+filename = "After Dark.mp3"
+
+data, samplerate = sf.read(filename)
+
+print(f"Playing {filename}")
+sd.play(data, samplerate)
+sd.wait()
+
+print("Finished")
