a8e64f1e9d6294eb66ccbb4e3d551d18880c70dd
Nizanth NS <nizanthns10@gmail.com>
2026-06-08 19:41:04 +0530

    Add files via upload

---
+++ 66. API.py
@@ -0,0 +1,33 @@
+# API Using Python
+
+import requests
+
+# https://pokeapi.co/api/v2/
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url = f"{base_url}pokemon/{name}"
+    response = requests.get(url)
+    # print(response)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+# print("Data retrieved")
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+
+
+pokemon_name = "Onix"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+try:
+    if pokemon_info:
+        print(f"Name : {pokemon_info["name"].capitalize()}")
+        print(f"ID : {pokemon_info["id"]}")
+        print(f"Height : {pokemon_info["height"]}")
+        print(f"Weight : {pokemon_info["weight"]}")
+
+except KeyError:
+    print("Failed to retrieve data")
